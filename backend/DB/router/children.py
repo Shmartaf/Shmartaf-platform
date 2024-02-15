178e88fc1e34bd8b8f1c7ed650eef8f4de674a40
Or Basker <or.basker@ridewithvia.com>
2024-02-15 20:17:26 +0200

    updated from api, created royer for: 1) Users - full and ready 2) Parents -not full but working with user information as well for read 3) Babysotters - User+ babysitter info full missing from skills attached as well 4) Child - full info without parents of the children and skills

--- backend/DB/router/children.py
+++ backend/DB/router/children.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
 from typing import List
 
-from database import SessionLocal
 import crud
 import schemas
+from database import SessionLocal
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 
 router = APIRouter()
+
 
 def get_db():
     db = SessionLocal()
@@ -15,12 +16,14 @@ def get_db():
     finally:
         db.close()
 
+
 @router.post("/", response_model=schemas.Children)
 def create_children(children: schemas.Children, db: Session = Depends(get_db)):
     db_children = crud.get_children(db, children_id=children.childrenid)
     if db_children:
         raise HTTPException(status_code=400, detail="Children already registered")
     return crud.create_children(db=db, children=children)
+
 
 @router.get("/{children_id}", response_model=schemas.ChildrenRead)
 def read_children(children_id: int, db: Session = Depends(get_db)):
@@ -29,9 +32,8 @@ def read_children(children_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Children not found")
     return db_children
 
+
 @router.get("/", response_model=List[schemas.ChildrenRead])
 def read_childrens(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     childrens = crud.get_childrens(db, skip=skip, limit=limit)
     return childrens
-
-
