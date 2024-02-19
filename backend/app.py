f4b3ef693c18fe6d4950397d585bbc3f8713a519
Or Basker <or.basker@ridewithvia.com>
2024-02-19 10:57:47 +0200

    added logger

--- backend/app.py
+++ backend/app.py
@@ -3,16 +3,21 @@ from database.database import Database
 from fastapi import FastAPI
 
 # from router import babysitter, children, parent, users, reviews, scheduler
-from router import babysitter, parent, reviews, users
+from router import babysitter, parent, reviews, scheduler, users
 
 Database().create_all()
 
-app = FastAPI()
+app = FastAPI(
+    debug=True,
+    title="Shmartaf App",
+    description="This is a simple babysitter app",
+    version="0.1",
+)
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(parent.router, prefix="/parents", tags=["parents"])
 app.include_router(babysitter.router, prefix="/babysitters", tags=["babysitters"])
 app.include_router(reviews.router, prefix="/reviews", tags=["reviews"])
-# app.include_router(scheduler.router, prefix="/schedules", tags=["schedules"])
+app.include_router(scheduler.router, prefix="/schedules", tags=["schedules"])
 
 if __name__ == "__main__":
     uvicorn.run(
--- backend/router/users.py
+++ backend/router/users.py
@@ -4,9 +4,10 @@ from fastapi import APIRouter, HTTPException
 
 from backend.database import models, schemas
 from backend.database.dal import DataAccessLayer
+from backend.logger import ColorLogger
 
 router = APIRouter()
-
+logger = ColorLogger()
 
 dal = DataAccessLayer()
 
@@ -14,6 +15,7 @@ dal = DataAccessLayer()
 @router.get("/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 10):
     users = dal.get_all(models.User, skip=skip, limit=limit)
+    logger.log("Users has retrieved succesfuly", "INFO", data=users)
     return users
 
 
@@ -21,6 +23,7 @@ def read_users(skip: int = 0, limit: int = 10):
 def read_user(user_id: int):
     user = dal.get(models.User, user_id)
     if user is None:
+        logger.log(f"User not found {user_id}", "ERROR", data=user)
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
