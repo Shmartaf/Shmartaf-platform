858d1b46393ff001ff64b219a1cc5669039ec9c2
galavital13 <gal13031993@gmail.com>
2024-02-23 17:54:04 +0200

    front

--- shmartaf/src/pages/Favorites.jsx
+++ shmartaf/src/pages/Favorites.jsx
@@ -1,36 +1,34 @@
-import { Card } from "@mui/material";
 import Box from "@mui/material/Box";
 import BabysitterCard from "../components/BabysitterCard";
-import Header from "../components/Header";
 // impport api
 
 // babysitters = api.get("/babysitters)
 const babysitters = [
   {
-    name: "Odel Levi",
-    image: "https://i.pravatar.cc/300?img=1",
-    desc: "Sociable, athletic, loves animals, available frequently, patient.",
-    location: "Tel Aviv",
-    rating: "4.50",
+    name: "",
+    image: "",
+    desc: "",
+    location: "",
+    rating: "",
   },
   {
-    name: "Shai Damari",
-    image: "https://i.pravatar.cc/300?img=2",
-    desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    location: "Haifa",
-    rating: "4.75",
+    name: "",
+    image: "",
+    desc: "",
+    location: "",
+    rating: "",
   },
   {
-    name: "Nurit Levi",
-    image: "https://i.pravatar.cc/300?img=3",
-    rating: "3.90",
-    desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    name: "",
+    image: "",
+    rating: "",
+    desc: "",
   },
   {
-    name: "Tom Levi",
-    image: "https://i.pravatar.cc/300?img=4",
-    rating: "3.50",
-    desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    name: "",
+    image: "",
+    rating: "",
+    desc: "",
   },
 ];
 
