5e8054f3ffa26437b0465cc6554167f38113f385
galavital13 <gal13031993@gmail.com>
2024-03-01 18:08:09 +0200

    new

--- shmartaf/README.md
+++ shmartaf/README.md
@@ -9,3 +9,41 @@ mui (https://mui.com/material-ui/getting-started/installation/)
 
 icons
 https://mui.com/material-ui/material-icons/
+
+
+figmae:
+https://www.figma.com/file/UYHeq7eVGjTXWQ0YReLLql/Shmartaf?type=design&node-id=0-1&mode=design&t=6EdiyQFwkzwTAGUw-0
+
+
+dont forget to change URL AND PORT!
+
+I had to allow CORS so changed the app.py
+
+import uvicorn
+from database.database import Database
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+------ADD THIS ------
+from fastapi.middleware.cors import CORSMiddleware
+---------------------
+from router import babysitter, certifications, parent, requirements, users
+
+from database.models import Base
+
+app = FastAPI(
+    debug=True,
+    title="Shmartaf App",
+    description="This is a simple babysitter app",
+    version="0.1",
+)
+------AND THIS -----
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Set this to your frontend URL in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+diffrent routes for parent / babysitter
+fetch babysitters from API
--- shmartaf/src/App.jsx
+++ shmartaf/src/App.jsx
@@ -1,30 +1,57 @@
 import { Route, Routes } from "react-router-dom";
 
 import Layout from "./components/Layout";
-import Dashboard from "./pages/Dashboard";
+import ParentDashboard from "./pages/ParentDashboard";
 import Find from "./pages/Find";
 import Contacted from "./pages/Contacted";
 import Notifications from "./pages/Notifications";
 import Favorites from "./pages/Favorites";
 import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
+import Reviews from "./pages/Reviews";
+import Schedule from "./pages/Schedule";
+import { BabysitterContext } from "./context/BabysitterContext";
+import { useContext } from "react";
+import Login from "./pages/Login";
+import BabysitterDashboard from "./pages/BabysitterDashboard";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/find" element={<Find />} />
-        <Route path="/contacted" element={<Contacted />} />
-        <Route path="/notifications" element={<Notifications />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/settings" element={<Settings />} />
-      </Route>
-      {/* <Route path="/find/:id" element={<Details />} /> */}
-    </Routes>
-  );
+  const { state, dispatch } = useContext(BabysitterContext);
+
+  if (!state?.user?.role) {
+    return <Login />;
+  } else {
+    return (
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            path="/"
+            element={
+              state?.user?.role === "babysitter" ? (
+                <BabysitterDashboard />
+              ) : (
+                <ParentDashboard />
+              )
+            }
+          />
+          <Route path="/parentdashboard" element={<ParentDashboard />} />
+          <Route
+            path="/babysitterdashboard"
+            element={<BabysitterDashboard />}
+          />
+          <Route path="/find" element={<Find />} />
+          <Route path="/contacted" element={<Contacted />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} />
+        </Route>
+        {/* <Route path="/find/:id" element={<Details />} /> */}
+      </Routes>
+    );
+  }
 }
 
 export default App;
